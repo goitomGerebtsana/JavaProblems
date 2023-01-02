934c837a22a35a81304a309b3c7e990db5aa2b1d
Goitom Kebede <58806509+goitomGerebtsana@users.noreply.github.com>
2023-01-02 11:10:33 -0800

    Update Evens.java

--- Evens.java
+++ Evens.java
@@ -1,3 +1,10 @@
+/*An Evens number is an integer whose digits are all even. For example 2426 is an Evens number 
+but 3224 is not.
+Write a function named isEvens that returns 1 if its integer argument is an Evens number otherwise it 
+returns 0.
+*/
+
+
 public class Evens {
 
     public static void main(String args[]) {
